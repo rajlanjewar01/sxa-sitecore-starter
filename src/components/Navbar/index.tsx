6b1088910ad02962315c5f0b280ba819b506550b
rajlanjewar01 <rajl.perficient@gmail.com>
2026-01-04 19:13:25 +0530

    Custom hooks - media query

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,85 +4,93 @@ import { NavItem } from './types';
 import { MenuItem } from './MenuItem';
 import { SubMenu } from './SubMenu';
 import { NavProvider, useNav } from './NavContext';
+import { useMediaQuery } from './hooks/useMediaQuery'; 
+
 import styles from './Navbar.module.scss';
 
 type NavbarProps = ComponentProps & {
-  fields: { navItems: NavItem[] };
+	fields: { navItems: NavItem[] };
 };
 
 const NavbarContent = ({ navItems }: { navItems: NavItem[] }) => {
-  const { 
-    activeTabId, setActiveTabId, 
-    isOpen, setIsOpen, 
-    isMobileMenuOpen, setIsMobileMenuOpen 
-  } = useNav();
+	const { 
+		activeTabId, setActiveTabId, 
+		isOpen, setIsOpen, 
+		isMobileMenuOpen, setIsMobileMenuOpen 
+	} = useNav();
 
-  const activeItem = useMemo(() => 
-    navItems.find((item) => item.id === activeTabId), 
-    [navItems, activeTabId]
-  );
+	const isDesktop = useMediaQuery('(min-width: 1025px)');
 
-  const handleMouseLeave = () => {
-    if (window.innerWidth > 1024) {
-      setIsOpen(false);
-      setActiveTabId(null);
-    }
-  };
+	const activeItem = useMemo(() => 
+		navItems.find((item) => item.id === activeTabId), 
+		[navItems, activeTabId]
+	);
 
-  return (
-    <header 
-      className={`${styles.navWrapper} ${isMobileMenuOpen ? styles.navWrapper_expanded : ''}`} 
-      onMouseLeave={handleMouseLeave}
-    >
-      <nav className={styles.navbar}>
-        {isMobileMenuOpen && activeTabId && (
-          <button className={styles.backButton} onClick={() => setActiveTabId(null)}>
-            <div className={styles.backArrow} />
-          </button>
-        )}
+	const handleMouseLeave = () => {
+		if (isDesktop) {
+			setIsOpen(false);
+			setActiveTabId(null);
+		}
+	};
 
-        <button className={styles.hamburger} onClick={() => {
-          setIsMobileMenuOpen(!isMobileMenuOpen);
-          if (isMobileMenuOpen) setActiveTabId(null);
-        }}>
-          <span className={isMobileMenuOpen ? styles.hamburger_iconOpen : styles.hamburger_icon} />
-          <span className={isMobileMenuOpen ? styles.hamburger_iconOpen : styles.hamburger_icon} />
-        </button>
+	return (
+		<header 
+			className={`${styles.navWrapper} ${isMobileMenuOpen ? styles.navWrapper_expanded : ''}`} 
+			onMouseLeave={handleMouseLeave}
+		>
+			<nav className={styles.navbar}>
+				{isMobileMenuOpen && activeTabId && (
+					<button className={styles.backButton} onClick={() => setActiveTabId(null)}>
+						<div className={styles.backArrow} />
+					</button>
+				)}
 
-        <div className={`
-          ${styles.menuSlider} 
-          ${isMobileMenuOpen ? styles.menuSlider_visible : ''} 
-          ${activeTabId ? styles.menuSlider_viewSub : ''}
-        `}>
+				<button className={styles.hamburger} onClick={() => {
+						setIsMobileMenuOpen(!isMobileMenuOpen);
+						if (isMobileMenuOpen) setActiveTabId(null);
+					}}>
+					<span className={isMobileMenuOpen ? styles.hamburger_iconOpen : styles.hamburger_icon} />
+					<span className={isMobileMenuOpen ? styles.hamburger_iconOpen : styles.hamburger_icon} />
+				</button>
 
-          <ul className={styles.navbar__list}>
-            {navItems.map((item) => (
-              <MenuItem key={item.id} item={item} />
-            ))}
-          </ul>
+				<div className={`
+					${styles.menuSlider} 
+					${isMobileMenuOpen ? styles.menuSlider_visible : ''} 
+					${activeTabId ? styles.menuSlider_viewSub : ''}
+				`}>
 
-          <div className={styles.mobileSubView}>
-            {activeItem && <SubMenu columns={activeItem.fields.columns} />}
-          </div>
-        </div>
-      </nav>
+					<ul className={styles.navbar__list}>
+						{navItems.map((item) => (
+							<MenuItem key={item.id} item={item} />
+						))}
+					</ul>
 
-      <div className={`${styles.megaMenu} ${isOpen ? styles.megaMenu_open : ''}`}>
-        {activeItem && <SubMenu columns={activeItem.fields.columns} />}
-      </div>
+					<div className={styles.mobileSubView}>
+						{activeItem && <SubMenu columns={activeItem.fields.columns} />}
+					</div>
+				</div>
+			</nav>
 
-      <div 
-        className={`${styles.overlay} ${isOpen ? styles.overlay_visible : ''}`}
-        onMouseEnter={handleMouseLeave}
-      />
-    </header>
-  );
+			<div className={`${styles.megaMenu} ${isOpen && isDesktop ? styles.megaMenu_open : ''}`}>
+				{activeItem && <SubMenu columns={activeItem.fields.columns} />}
+			</div>
+
+			<div 
+				className={`${styles.overlay} ${isOpen && isDesktop ? styles.overlay_visible : ''}`}
+				onMouseEnter={handleMouseLeave}
+			/>
+		</header>
+	);
 };
 
-const AppleNavbar = ({ fields }: NavbarProps) => (
-  <NavProvider>
-    <NavbarContent navItems={fields?.navItems || []} />
-  </NavProvider>
-);
+const AppleNavbar = ({ fields }: NavbarProps): JSX.Element => {
+	const items = fields?.navItems || [];
+
+	return (
+		<NavProvider>
+			<NavbarContent navItems={items} />
+		</NavProvider>
+	);
+};
 
 export default AppleNavbar;
